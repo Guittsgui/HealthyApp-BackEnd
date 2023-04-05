01d0c645721789014a1e664de402bc327c996046
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-04-05 18:41:31 -0300

    rotas diet criadas

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import UserController from './controllers/UserController.js'
-
+import DietController from "./controllers/DietController.js";
 
 const router = Router()
 
@@ -8,4 +8,8 @@ router.get('/', UserController.index)
 router.get('/user/:id', UserController.show)
 router.post('/user',UserController.store)
 router.post('/login',UserController.verifyLogin)
+
+router.post('/diet', DietController.store)
+router.get('/diet/:id', DietController.findDietByUserId)
+router.delete('/diet/:id',DietController.delete)
 export default router;
