9f5d925105a9201747451a6851d5a4a7b38ca165
Guilherme Tavares <guilhermet.silva@viannasempre.com.br>
2023-04-05 22:32:04 -0300

    post model criado

---
+++ src/ model/PostModel.js
@@ -0,0 +1,34 @@
+import {sequelize} from '../instances/mysql.js'
+import { DataTypes } from "sequelize"
+
+export const Post = sequelize.define("Post",{
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        required:true
+    },
+    title:{
+        type: DataTypes.STRING,
+        required: true
+    },
+    type:{
+        type: DataTypes.STRING,
+        required: true
+    },
+    content:{
+        type: DataTypes.STRING,
+        required: true
+    },
+    url:{
+        type: DataTypes.STRING,
+        required: true
+    },
+    likes:{
+        type: DataTypes.INTEGER,
+    }
+
+},{
+    tableName: "blog",
+    timestamps: false
+})
